97925264398fffcfd94ab9c64eb7ea8d6533cc6b
irfnhanif <14.irfanh@gmail.com>
2024-09-21 11:53:24 +0700

    Refactor App component to use arrow function syntax and add calculateDrops function

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,29 @@ interface DropResults {
   range9to11: string
 }
 
-function App: React.FC() {
+const App: React.FC = () => {
   const [price, setPrice] = useState<string>('')
   const [results, setResults] = useState<DropResults| null>(null)
 
+  const calculateDrops = (): void => {
+    const inputPrice = parseFloat(price)
+
+    if (!isNaN(inputPrice)) {
+      const dropPrice = (percent: number): string => (inputPrice - inputPrice * percent).toFixed(2)
+
+      const drop5 = dropPrice(0.05)
+      const drop10 = dropPrice(0.1)
+      const range4to6 = `${dropPrice(0.04)} - ${dropPrice(0.06)}`
+      const range9to11 = `${dropPrice(0.09)} - ${dropPrice(0.11)}`
+
+      setResults({
+        drop5,
+        drop10,
+        range4to6,
+        range9to11
+      })
+    }
+  }
   return (
     <>
     </>
